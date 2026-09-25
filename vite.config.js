84b7e4dad66909e4ddbd754042078d4435b3865c
Jan Czajnik <janczajnik1776@gmail.com>
2026-09-25 15:48:23 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EparPredictions/', // must match your GitHub repo name exactly, with leading/trailing slashes
 })
